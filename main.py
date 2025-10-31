194954cf382fc7cfa732714e4394d257148a475f
youness boumeshouli <boumeshouliyouness6@gmail.com>
2025-10-31 12:21:10 +0100

    project

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path='chromedriver.exe')  # Open Chrome browser
+driver.get('https://fbref.com/en/squads/822bd0ba/2024-2025/Liverpool-Stats')  # Navigate to a URL
